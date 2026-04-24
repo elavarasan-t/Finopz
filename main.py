0f123b3c6fdd0c82ce3029c091c3e23f4db74306
Elavarasan t <elavarasan.t@postiefs.com>
2026-04-24 18:06:50 +0530

    api key generation and validation are commented

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from utils import limiter
 from src.routes.Azure import router as azure_router
-from src.routes.APIKEY import router as api_key_router
+
+#from src.routes.APIKEY import router as api_key_router
 app = FastAPI()
 
 app.state.limiter = limiter
@@ -10,6 +11,6 @@ API_PREFIX = "/api"
 
 app.include_router(azure_router, prefix=API_PREFIX)
 
-app.include_router(api_key_router, prefix=API_PREFIX)
+#app.include_router(api_key_router, prefix=API_PREFIX)
 
 
